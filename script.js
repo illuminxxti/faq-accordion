bd3c29341faa177e05dcc69a2e2d9299a6a9ca52
Adarsh Suresh <113850129+illuminxxti@users.noreply.github.com>
2024-02-10 20:42:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ answers.forEach((event) => {
         let imgClose = event.querySelector('.img-close');
         if (event.classList.contains("active")) {
             event.classList.remove("active");
-            imgClose.src = "/assets/images/icon-plus.svg";
+            imgClose.src = "icon-plus.svg";
         } else {
             event.classList.add("active");
-            imgClose.src = "/assets/images/icon-minus.svg";
+            imgClose.src = "icon-minus.svg";
         }
     })
 });
